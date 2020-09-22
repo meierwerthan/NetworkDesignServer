255e950e373bc4b48cefa6988ccfca3e9982b3d4
meierwerthan <Meier.Werthan@du.edu>
2020-09-22 10:41:02 -0600

    first

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, async() => {
     console.log(`Listening on port ${port}`)
